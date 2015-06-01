713b43555007ed0e6cb603bd8f58bcf3c83e32b2
Dennis Fischer <fischer_dennis@live.de>
2015-06-02 00:09:08 +0200

    Initial commit

---
+++ Assembly2/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assembly2
+{
+    public struct Foo
+    {
+
+    }
+}
---
+++ RoslynBug/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Immutable;
+using Assembly2;
+
+public class Class1
+{
+    static unsafe void Main()
+    {
+        System.Console.WriteLine(sizeof(Foo));
+    }
+}
